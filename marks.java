b644ecc8763c35cdedb2512bd854374c61094e92
Gaurav Jain <gauravrohinda0@gmail.com>
2026-04-07 14:10:33 +0530

    all clean

--- marks.java
+++ marks.java
@@ -1,8 +1,8 @@
-class marks{
-    public static void main(String args[]){
-         int maxScore = 620 ;
-         float userScore = 420 ;
-         double percert = (float) userScore / maxScore * 100 ;
-         System.out.println("user marks : " + percert );
+class marks {
+    public static void main(String args[]) {
+        int maxScore = 620;
+        float userScore = 419;
+        double percert = (float) userScore / maxScore * 100;
+        System.out.println("user marks : " + percert);
     }
 }
